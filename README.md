cfee22f082535c19b44ed0bf7e3f19ed3e39a37e
pjwilcoxen <wilcoxen@syr.edu>
2022-04-20 00:40:19 -0400

    Initial version.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# x430-distance
-Example: GIS Distance Operations
+# Example: GIS Distance Operations
+
+## Summary
+
+The **demo.py** file in this repository builds several GIS layers that can be used with QGIS in distance-based calculations such as building Voronoi polygons or computing the shortest line between features.
+
+## Input Data
+
+There are two input files: **demo.gpkg**, which has a number of Census layers related to Syracuse and Onondaga County, and **Retail_Food_Stores.csv**, which is a list of all retail food stores licensed by the NYS Department of Agriculture and Markets. The original database can be found here:  <https://data.ny.gov/Economic-Development/Retail-Food-Stores/9a8c-vfzj>.
+
+## Deliverables
+
+None. This is an example only and there's nothing due.
+
+## Instructions
+
+1. Run demo.py
+
+1. Load `demo-output.gpkg` into QGIS
+
+1. Try building Voronoi polygons. The Voronoi tool is under `Vector` > `Geometry Tools`.
+
+1. Try computing the shortest distance between features. The shortest line tool is available in the `Toolbox` (gear icon) under the `Vector analysis` heading.
---
+++ demo.py
@@ -0,0 +1,131 @@
+"""
+demo.py
+Spring 2022 PJW
+
+Demonstrate use of Voronoi polygons and nearest feature
+calculations. Requires QGIS for some operations.
+"""
+
+import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+out_file = 'demo-output.gpkg'
+
+#
+#  Set up the store input file, the CRS used by the store file,
+#  which is WGS 84, and the CRS that will be used for projected 
+#  data, which is UTM 18N
+#
+
+store_file = 'Retail_Food_Stores.csv'
+
+wgs84 = 4326
+utm18n = 26918
+
+#
+#  Read the store file, filter down to Onondaga County, and 
+#  filter out records with no coordinates
+#
+
+raw = pd.read_csv(store_file)
+
+trim = raw.query("County == 'Onondaga'")
+trim = trim.dropna(subset='Georeference').copy()
+
+#%%
+#
+#  Filter store data down to large stores
+#
+
+big_stores = [
+    'ALDI',
+    'BJS WHOLESALE',
+    'COSTCO',
+    'PRICE CHOPPER',
+    'TARGET',
+    'TOPS',
+    'TRADER JOES',
+    'WAL-MART',
+    'WALMART',
+    'WEGMANS'
+    ]
+
+trim['big'] = False
+
+for store in big_stores:
+
+    is_big = trim['DBA Name'].str.startswith(store)
+    is_gas = trim['DBA Name'].str.contains('XPRESS')
+
+    this_store = (is_big == True) & (is_gas == False)
+
+    trim['big'] = trim['big'] | this_store
+  
+big = trim[ trim['big'] ]
+
+#%%
+#
+#  Now build a GeoSeries from the georeference column,
+#  which uses the Well-Known Text representation of 
+#  store coordinates. Then build a full GeoDataFrame
+#  using it.
+#
+
+coords = gpd.GeoSeries.from_wkt(big['Georeference'])
+
+data_only = big.drop(columns='Georeference')
+geo = gpd.GeoDataFrame( data=data_only, geometry=coords, crs=wgs84)
+
+#
+#  Now convert the data to UTM 18N
+#
+
+geo = geo.to_crs(utm18n)
+
+#%%
+#
+#  Load the county and city polygons for convenience, then plot
+#  a figure
+#
+
+county = gpd.read_file('demo.gpkg',layer='county')
+syr = gpd.read_file('demo.gpkg',layer='city')
+
+fig,ax1 = plt.subplots(dpi=300)
+county.plot(color='gray',ax=ax1)
+syr.plot(color='tan',ax=ax1)
+geo.plot(color='blue',markersize=1,ax=ax1)
+ax1.axis('off')
+
+#%%
+#
+#  Build a geopackage for use with QGIS in building Voronoi 
+#  polygons. Start by removing any existing version of the 
+#  output file, then write the layers.
+#
+
+if os.path.exists(out_file):
+    os.remove(out_file)
+    
+county.to_file(out_file,layer='county',index=False)
+syr.to_file(out_file,layer='city',index=False)
+geo.to_file(out_file,layer='stores',index=False)
+
+#%%
+#
+#  Copy a couple of other layers across that will be convenient
+#  in demonstrating features of QGIS. Also, build a layer 
+#  of tract centroids in the process.
+#
+
+roads = gpd.read_file('demo.gpkg',layer='roads')
+roads.to_file(out_file,layer='roads',index=False)
+
+tracts = gpd.read_file('demo.gpkg',layer='tracts')
+tracts.to_file(out_file,layer='tracts',index=False)
+
+centroids = tracts.copy()
+centroids['geometry'] = tracts.centroid
+centroids.to_file(out_file,layer='centroids',index=False)
